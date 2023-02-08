0f838dd12bb6d35586cf6d3dec0f71a8d7f46a8d
paullpp <71856130+paullpp@users.noreply.github.com>
2023-02-08 12:21:12 -0800

    current progress

--- ece375-L3_skeleton.asm
+++ ece375-L3_skeleton.asm
@@ -12,6 +12,7 @@
 ;*	Internal Register Definitions and Constants
 ;***********************************************************
 .def	mpr = r16				; Multipurpose register is required for LCD Driver
+.def	counter = r17
 .equ	PD_seven = 7
 .equ	PD_five = 5
 .equ	PD_four = 4
@@ -33,10 +34,10 @@
 ;*	Program Initialization
 ;***********************************************************
 INIT:							; The initialization routine
-		ldi		mpr, low(RAMEND)
-		out		SPL, mpr		; Load SPL with low byte of RAMEND
-		ldi		mpr, high(RAMEND)
-		out		SPH, mpr		; Load SPH with high byte of RAMEND
+		ldi	mpr, low(RAMEND)	; Initialize Stack Pointer
+		out SPL, mpr
+		ldi mpr, high(RAMEND)
+		out SPH, mpr
 
 		rcall LCDInit			; Initialize LCD Display
 
@@ -44,8 +45,21 @@ INIT:							; The initialization routine
 		out DDRD, mpr
 		ldi mpr, $FF
 		out PORTD, mpr
-		
-		rcall LCDClr			; Clear LCD
+
+		ldi mpr, $FF
+		out DDRB, mpr
+		ldi mpr, $00
+		out PORTB, mpr
+
+		rcall LCDClr
+
+		ldi ZL, low(STRING_BEG_L1<<1)
+		ldi ZH, high(STRING_BEG_L1<<1)
+
+		ldi YL, $00
+		ldi YH, $01
+
+		ldi counter, 9
 
 		; NOTE that there is no RET or RJMP from INIT,
 		; this is because the next instruction executed is the
@@ -59,14 +73,40 @@ MAIN:							; The Main program
 
 		in mpr, PIND
 		andi mpr, (1<<PD_seven|1<<PD_five|1<<PD_four)
-		cpi mpr, (1<<PD_seven)		; Check if pd7 is hit
-		brne NEXT1
+
+		lpm mpr, Z+
+		st Y+, mpr
+		dec counter
+		brne MAIN
+
+		rcall LCDWrLn1
+
+NEXT:
+
+		ldi ZL, low(STRING_BEG_L2<<1)
+		ldi ZH, high(STRING_BEG_L2<<1)
+
+		ldi YL, $10
+		ldi YH, $01
+
+		ldi counter, 12
+
+		;cpi mpr, (1<<PD_seven)		; Check if pd7 is hit
+		lpm mpr, Z+
+		st Y+, mpr
+		dec counter
+		brne NEXT
 		;wrap around
+
+		rcall LCDWrLn2
+		rjmp MAIN
 
 NEXT1:	cpi mpr, (1<<PD_five)
 		brne NEXT2
 		;name and hello world
 		rcall WORLD
+
+		rjmp MAIN
 
 NEXT2:  cpi mpr, (1<<PD_four)
 		brne MAIN
@@ -93,13 +133,33 @@ NEXT2:  cpi mpr, (1<<PD_four)
 ;-----------------------------------------------------------
 CLEAR:	
 		rcall LCDClr
+		out PORTB, mpr
 
 		ret						; End a function with RET
 
 WORLD:
-	
-		ldi ZL, low(STRING_BEG<<1)
-		ldi ZH, high(STRING_BEG<<1)
+		;move stuff to data memory
+		;line 1 $0100 - $010F
+		;line 2 $0110 - $011F
+
+		;ldi mpr, 101
+		;ldi XL, low($0100)
+		;ldi XH, high($0100)
+
+		ldi ZL, low(STRING_BEG_L1<<1)
+		ldi ZH, high(STRING_BEG_L1<<1)
+
+		ldi mpr, (1<<7)
+		out PORTB, mpr
+
+		;rcall Bin2ASCII
+
+		;LPM mpr, Z+
+
+		;ldi YL, $00
+		;ldi YH, $01
+		;ld r16, Y+
+		;st Y, r17
 
 		lpm r15, Z+
 		lpm r16, Z+
@@ -107,10 +167,13 @@ WORLD:
 		ldi YL, $00
 		ldi YH, $01
 
-		st Y, r16
+		st Y+, r16
 
 		rcall LCDWrLn1
 
+		ret
+		;rcall LCDWrLn2
+		
 
 ;***********************************************************
 ;*	Stored Program Data
@@ -120,11 +183,17 @@ WORLD:
 ; An example of storing a string. Note the labels before and
 ; after the .DB directive; these can help to access the data
 ;-----------------------------------------------------------
-STRING_BEG:
-.DB		"My Test String"		; Declaring data in ProgMem
-STRING_END:
+STRING_BEG_L1:
+.DB		"Paul Lipp"
+STRING_BEG_L2:
+.DB		"Ryan Muriset"		; Declaring data in ProgMem	9
+STRING_END_1:
+;STRING_BEG_L2:
+;.DB		"Ryan Muriset"	;	12
+;STRING_END_L2:
 
 ;***********************************************************
 ;*	Additional Program Includes
 ;***********************************************************
 .include "LCDDriver.asm"		; Include the LCD Driver
+
