320ed1c6d16db8f01200544db0e940d0d3732e86
paullpp <71856130+paullpp@users.noreply.github.com>
2023-02-08 11:32:24 -0800

    add current progress

--- ece375-L3_skeleton.asm
+++ ece375-L3_skeleton.asm
@@ -12,6 +12,9 @@
 ;*	Internal Register Definitions and Constants
 ;***********************************************************
 .def	mpr = r16				; Multipurpose register is required for LCD Driver
+.equ	PD_seven = 7
+.equ	PD_five = 5
+.equ	PD_four = 4
 
 ;***********************************************************
 ;*	Start of Code Segment
@@ -30,9 +33,17 @@
 ;*	Program Initialization
 ;***********************************************************
 INIT:							; The initialization routine
-		; Initialize Stack Pointer
+		ldi	mpr, low(RAMEND)	; Initialize Stack Pointer
+		out SPL, mpr
+		ldi mpr, high(RAMEND)
+		out SPH, mpr
 
-		; Initialize LCD Display
+		rcall LCDInit			; Initialize LCD Display
+
+		ldi mpr, $00			; Initialize Port D for input
+		out DDRD, mpr
+		ldi mpr, $FF
+		out PORTD, mpr
 
 		; NOTE that there is no RET or RJMP from INIT,
 		; this is because the next instruction executed is the
@@ -43,6 +54,22 @@ INIT:							; The initialization routine
 ;***********************************************************
 MAIN:							; The Main program
 		; Main function design is up to you. Below is an example to brainstorm.
+
+		in mpr, PIND
+		andi mpr, (1<<PD_seven|1<<PD_five|1<<PD_four)
+		cpi mpr, (1<<PD_seven)		; Check if pd7 is hit
+		brne NEXT1
+		;wrap around
+
+NEXT1:	cpi mpr, (1<<PD_five)
+		brne NEXT2
+		;name and hello world
+		rcall WORLD
+
+NEXT2:  cpi mpr, (1<<PD_four)
+		brne MAIN
+		rcall CLEAR
+
 
 		; Move strings from Program Memory to Data Memory
 
@@ -62,15 +89,45 @@ MAIN:							; The Main program
 ; Desc: Cut and paste this and fill in the info at the
 ;		beginning of your functions
 ;-----------------------------------------------------------
-FUNC:							; Begin a function with a label
-		; Save variables by pushing them to the stack
-
-		; Execute the function here
-
-		; Restore variables by popping them from the stack,
-		; in reverse order
+CLEAR:	
+		rcall LCDClr
 
 		ret						; End a function with RET
+
+WORLD:
+		;move stuff to data memory
+		;line 1 $0100 - $010F
+		;line 2 $0110 - $011F
+
+		;ldi mpr, 101
+		;ldi XL, low($0100)
+		;ldi XH, high($0100)
+
+		ldi ZL, low(STRING_BEG<<1)
+		ldi ZH, high(STRING_BEG<<1)
+
+		;rcall Bin2ASCII
+
+		;LPM mpr, Z+
+
+		;ldi YL, $00
+		;ldi YH, $01
+		;ld r16, Y+
+		;st Y, r17
+
+		lpm r15, Z+
+		lpm r16, Z+
+
+		ldi YL, $00
+		ldi YH, $01
+
+		st Y, r16
+
+		rcall LCDWrLn1
+
+		ret
+		;rcall LCDWrLn2
+		
 
 ;***********************************************************
 ;*	Stored Program Data
