58d1072d7de18f922f98450d34e6c45932973abc
Ryan Muriset <rmuriset@gmail.com>
2023-02-24 12:39:49 -0800

    Update ece375-L6_skeleton.asm

--- ece375-L6_skeleton.asm
+++ ece375-L6_skeleton.asm
@@ -44,7 +44,10 @@ INIT:
 		ldi mpr, high(RAMEND)
 		out SPH, mpr
 		; Configure I/O ports
-
+		ldi mpr, $00			; Initialize Port D for input
+		out DDRD, mpr
+		ldi mpr, $FF
+		out PORTD, mpr
 		; Configure External Interrupts, if needed
 
 		; Configure 16-bit Timer/Counter 1A and 1B
